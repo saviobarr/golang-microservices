1630156f13c9e2c2b51af561ee43812895c227cd
saviobarr <saviobarr@gmail.com>
2023-11-17 10:38:37 -0300

    recover

--- mvc/main.go
+++ mvc/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/saviobarr/golang-microservices/mvc/app"
 )
 
 func main() {
 	app.StartApp()
+	defer fmt.Println("World")
+	fmt.Println("Hello")
+
+	defer func() {
+		recover()
+		fmt.Println("panic recovered")
+	}()
+
+	panic("panic")
 
 }
